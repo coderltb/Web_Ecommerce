1c333cc5c76da614a16c91c44ea8c4aee97618d8
Trần Bảo Long <lockhanhlong007@gmail.com>
2023-03-05 21:39:13 +0700

    Update Code

---
+++ aspnet-core/src/Web_ECommerce.Admin.Application.Contracts/Permissions/Web_ECommerceAdminPermissionDefinitionProvider.cs
@@ -0,0 +1,20 @@
+﻿using Web_ECommerce.Localization;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+
+namespace Web_ECommerce.Admin.Permissions;
+
+public class Web_ECommerceAdminPermissionDefinitionProvider : PermissionDefinitionProvider
+{
+    public override void Define(IPermissionDefinitionContext context)
+    {
+        var myGroup = context.AddGroup(Web_ECommerceAdminPermissions.GroupName);
+        //Define your own permissions here. Example:
+        //myGroup.AddPermission(Web_ECommercePermissions.MyPermission1, L("Permission:MyPermission1"));
+    }
+
+    private static LocalizableString L(string name)
+    {
+        return LocalizableString.Create<Web_ECommerceResource>(name);
+    }
+}
---
+++ aspnet-core/src/Web_ECommerce.Admin.HttpApi.Host/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Web_ECommerce.Admin.Controllers;
+
+public class HomeController : AbpController
+{
+    public ActionResult Index()
+    {
+        return Redirect("~/swagger");
+    }
+}
